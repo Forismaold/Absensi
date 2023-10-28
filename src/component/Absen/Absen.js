39163136eabe93dc155095e5db35a3336bec868e
McWooden <garryf7887@gmail.com>
2023-10-28 11:33:34 +0700

    sudah mendapatkan koordinat sekolah

--- src/component/Absen/Absen.js
+++ src/component/Absen/Absen.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
-import { MapContainer, Marker, Popup, Rectangle, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Rectangle, TileLayer, Tooltip } from 'react-leaflet'
+import LoadingSvg from '../utils/LoadingIcon'
+
+const first = [-7.4822300,  110.2220029]
+const second = [-7.4820399, 110.2222523]
 
 export default function Absen() {
     return <div className="">
@@ -9,61 +13,68 @@ export default function Absen() {
 }
 
 const MyMap = () => {
-    const [firstCoordinate, setFirstCoordinate] = useState([-7.4825645,  110.2221515])
-    const [secondCoordinate, setSecondCoordinate] = useState([-7.4822054, 110.2221904])
-    const [center, setcenter] = useState([-7.4825645,  110.2221515])
+    const [firstCoordinate, ] = useState(first)
+    const [secondCoordinate, ] = useState(second)
+    const [userCoor, setUserCoor] = useState([0,0])
+    const [loadingUserCoor, setLoadingUserCoor] = useState(false)
 
     function getCurrentLocation(callback) {
         if (navigator.geolocation) {
+            setLoadingUserCoor(true)
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     const { latitude, longitude } = position.coords
                     const value = [latitude, longitude]
                     callback(value)
+                    setLoadingUserCoor(false)
                 },
                 (error) => {
                     console.error('Error getting location:', error)
+                    setLoadingUserCoor(false)
                 }
-            )
+                )
         } else {
-            alert('Geolocation is not supported by your browser.')
+            alert('Geolocation tidak didukung browsermu.')
+            setLoadingUserCoor(false)
         }
     }
 
     useEffect(() => {
-        console.log(firstCoordinate)
+        getCurrentLocation(setUserCoor)
     },[firstCoordinate])
-  
+
     return (
         <div>
-            <MapContainer center={firstCoordinate} style={{ height: '20rem', width: '100%' }} zoom={22} scrollWheelZoom={false}>
+            <MapContainer center={firstCoordinate} style={{ height: '20rem', width: '100%' }} zoom={18} scrollWheelZoom={false}>
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                <Rectangle
-                bounds={[firstCoordinate, secondCoordinate]}
-                color="blue"
-                fillOpacity={0.5}
-                />
-                <Marker position={center}>
-                    <Popup>
-                        Lokasimu
-                    </Popup>
+                    <Rectangle
+                        bounds={[firstCoordinate, secondCoordinate]}
+                        color="blue"
+                        fillOpacity={0.5}
+                    >
+                    <Tooltip sticky>
+                        Lokasi Absen disini
+                    </Tooltip>
+                </Rectangle>
+                <Marker position={userCoor}>
+                    <Tooltip>
+                        Lokasi kamu disini
+                    </Tooltip>
                 </Marker>
             </MapContainer>
             <div className='flex gap-2 flex-wrap mt-2 flex-col md:flex-row'>
                 <div className='flex flex-1 flex-col shadow rounded p-2'>
-                    <p>First</p>
-                    <span>{firstCoordinate[0] || ''}, {firstCoordinate[1] || ''}</span>
-                    <button className='rounded text-neutral-100 px-2 py-1 bg-indigo-600' onClick={() => getCurrentLocation(setFirstCoordinate)}>Refresh</button>
+                        <p>Lokasi absen</p>
+                        <span>{firstCoordinate[0] || ''}, {firstCoordinate[1] || ''}</span>
+                        <span>{secondCoordinate[0] || ''}, {secondCoordinate[1] || ''}</span>
                 </div>
                 <div className='flex flex-1 flex-col shadow rounded p-2'>
-                    <p>Center</p>
-                    <span>{center[0]}, {center[1]}</span>
-                    <button className='rounded text-neutral-100 px-2 py-1 bg-indigo-600' onClick={() => getCurrentLocation(setcenter)}>Refresh</button>
-                </div>
-                <div className='flex flex-1 flex-col shadow rounded p-2'>
-                    <p>Second</p>
-                    <span>{secondCoordinate[0]}, {secondCoordinate[1]}</span>
-                    <button className='rounded text-neutral-100 px-2 py-1 bg-indigo-600' onClick={() => getCurrentLocation(setSecondCoordinate)}>Refresh</button>
+                    <p>Lokasi kamu</p>
+                    <span>{userCoor[0]}, {userCoor[1]}</span>
+                    
+                    <button className='flex items-center justify-center rounded text-neutral-100 px-2 py-1 bg-indigo-600' onClick={() => getCurrentLocation(setUserCoor)}>
+                        {loadingUserCoor? <LoadingSvg/>:<span>Segarkan</span>}
+                    </button>
                 </div>
             </div>
         </div>
