7fb42852682e17e27a1da756136e570dd7911604
Huddin <96611341+McWooden@users.noreply.github.com>
2024-02-17 12:31:38 +0700

    Update coordinates.js
    test koordinat multimedia

--- src/redux/coordinates.js
+++ src/redux/coordinates.js
@@ -24,9 +24,9 @@ const coordinates = createSlice({
         // first: developerArea[0],
         // second: developerArea[1],
         // center: developerArea[1],
-        first: [-7.4821931, 110.2223684],
-        second: [-7.4826406, 110.2221829],
-        center: [-7.482137557891397, 110.22213944103149],
+        first: [-7,4823544, 110,2228273],
+        second: [-7.482062, 110.222529],
+        center: [-7,4822669, 110,2226735],
         user: null,
         focusOnLocation: null
     },
