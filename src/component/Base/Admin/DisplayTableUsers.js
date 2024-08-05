587bf9abdfa2db14289a97d29b47d64a6db003a6
McWooden <garryf7887@gmail.com>
2024-08-06 05:09:43 +0700

    styling class

--- src/component/Base/Admin/DisplayTableUsers.js
+++ src/component/Base/Admin/DisplayTableUsers.js
@@ -8,11 +8,14 @@ const classList = [
 
 export default function DisplayTableUsers() {
     const [selectedClass, setSelectedClass] = useState(null)
+    // const [isFetch, setIsFetch] = useState(false)
+
 
     return <div className="flex flex-col gap-2">
         <div className="flex flex-wrap">
+            <div className={`p-2 cursor-pointer click-animation border-b-2 ${!selectedClass && 'border-secondary text-secondary bg-quaternary'}`} onClick={()=>setSelectedClass(null)}>null</div>
             {classList.map((item, i) => Array.from({ length: item.classCount }, (_, index) => (
-                    <div key={index + 1} className={`p-2 ${selectedClass === `${item.classNumberRank}-${index + 1}` && 'bg-secondary'}`} onClick={()=>setSelectedClass(`${item.classNumberRank}-${index + 1}`)}>{item.classNumberRank}-{index + 1}</div>
+                    <div key={index + 1} className={`p-2 cursor-pointer click-animation border-b-2 ${selectedClass === `${item.classNumberRank}-${index + 1}` && 'border-secondary text-secondary bg-quaternary'}`} onClick={()=>setSelectedClass(`${item.classNumberRank}-${index + 1}`)}>{item.classNumberRank}-{index + 1}</div>
                 ))
             )}
         </div>
