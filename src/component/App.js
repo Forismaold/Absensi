7ccb5e1c18ef943064674547fc4abab63886f497
Forisma <148964964+Forisma@users.noreply.github.com>
2024-08-09 07:26:27 +0700

    gak keliatan

--- src/component/App.js
+++ src/component/App.js
@@ -8,7 +8,7 @@ function App() {
       <Navbar/>
       <Base/>
     <p className='text-neutral-200'>Bagi yang sudah naik kelas 11 dan informasi akunnya masih kelas 10, sistem buat edit informasi akunnya masih dalam tahap pengembangan ya :) jadi pakai akun yang lama tidak apa</p>
-    <p className='text-neutral-200'>- huddin tadi malam sakit ketiduran/p>
+    <p className='text-neutral-200'>- huddin tadi malam sakit ketiduran</p>
     </div>
   )
 }
