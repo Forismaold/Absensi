7c6e5d8f29795a38f367b1005af4573f85d53912
Forisma <148964964+Forisma@users.noreply.github.com>
2024-08-09 07:22:48 +0700

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className='relative bg-primary text-neutral-500 min-h-screen overflow-auto flex flex-col items-center'>
       <Navbar/>
       <Base/>
-    <p>Bagi yang sudah naik kelas 11 dan informasi akunnya masih kelas 10, sistem buat edit informasi akunnya masih dalam tahap pengembangan ya :) jadi pakai akun yang lama tidak apa</p>
+    <p className='text-neutral-200'>Bagi yang sudah naik kelas 11 dan informasi akunnya masih kelas 10, sistem buat edit informasi akunnya masih dalam tahap pengembangan ya :) jadi pakai akun yang lama tidak apa</p>
+    <p className='text-neutral-200'>- Developer Huddin</p>
     </div>
   )
 }
