85b1665fbfb152d493fc2d29044113654d2a23bb
McWooden <garryf7887@gmail.com>
2024-11-01 00:51:12 +0700

    uuuu

--- src/component/Base/GoldenQr/GoldenQr.js
+++ src/component/Base/GoldenQr/GoldenQr.js
@@ -1,5 +1,5 @@
 export default function GoldenQr() {
     return <div className="flex flex-col gap-2">
-        
+        <p>developer memindahkan kode qr ke halaman akun dan akan merilis fitur baru dengan golden qr, saatnya memindai qr emas untuk melakukan absensi tanpa akses lokasi, dapatkan qr di setiap absensi dari admin</p>
     </div>
 }
