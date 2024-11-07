66a3b020949499e680aa3fb55b68ff5648f56848
McWooden <garryf7887@gmail.com>
2024-11-08 00:56:03 +0700

    awoakok

---
+++ src/component/Base/Absen/DetailAbsen/ReminderWrongGrade.js
@@ -0,0 +1,31 @@
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
+export default function ReminderWrongGrade() {
+    const absensi = useSelector(state => state.source.absensi)
+    const account = useSelector(state => state.source.account)
+
+    useEffect(() => {
+        console.log('====================================');
+        console.log(absensi, account);
+        console.log('====================================');
+    })
+
+    if (!absensi) return
+
+    if (absensi?.allowedGrades.includes(account?.kelas)) return
+    
+    return <div className="flex flex-col gap-2 items-start justify-start bg-yellow-300/50 text-yellow-700 rounded p-2">
+        <div className="flex gap-2 items-center">
+            <FontAwesomeIcon icon={faTriangleExclamation}/>
+            <p>Perhatikan bahwa absensi ini untuk angkatan kelas {absensi?.allowedGrades?.join(', ') || ''} sedangkan data di akun kamu menunjukkan kelas {account?.kelas}{account?.nomorKelas}</p>
+        </div>
+        <div className="flex gap-2 items-center">
+            <FontAwesomeIcon icon={faTriangleExclamation}/>
+            <p>Silahkan edit data info kamu di halaman <Link to={'/akun'} className="underline cursor-pointer">akun</Link></p>
+        </div>
+    </div>
+}
