7042c61176e12062c60ca06d9c13e241cc4b6ce2
McWooden <garryf7887@gmail.com>
2025-02-12 21:52:19 +0700

    it is work?

--- src/component/Base/Absen/AbsenceMethod/AbsenceList.js
+++ src/component/Base/Absen/AbsenceMethod/AbsenceList.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setUserCoordinate } from "../../../../redux/coordinates"
 import LoadingIcon from "../../../utils/LoadingIcon"
@@ -51,19 +51,19 @@ export function AbsenceStandar() {
 
 export function AbsenceWatch() {
     const [isWatchPosition, setisWatchPosition] = useState(false)
-    const userCoordinate = useSelector(state => state.coordinates.user)
+
     const focusOnLocation = useSelector(state => state.map.focusOnLocation)
 
-    const [intervalWatchId, setIntervalWatchId] = useState(null)
+    const intervalWatchIdRef = useRef(null) // Use ref instead of state
 
     const dispatch = useDispatch()
 
     function handleWatchPosition() {
         if (isWatchPosition) {
             console.log('watch position shutting down')
-            navigator.geolocation.clearWatch(intervalWatchId)
+            navigator.geolocation.clearWatch(intervalWatchIdRef.current)
+            intervalWatchIdRef.current = null
             setisWatchPosition(false)
-            setIntervalWatchId(false)
         } else {
             console.log('watch position power up')
             getCurrentLocation()
@@ -77,7 +77,7 @@ export function AbsenceWatch() {
                 const { latitude, longitude } = position.coords
                 dispatch(setUserCoordinate([latitude,longitude]))
                 console.log('watched', [latitude,longitude])
-                if (userCoordinate) focusOnLocation(userCoordinate)
+                focusOnLocation([latitude,longitude])
             }
     
             const errorCallback = (error) => {
@@ -89,66 +89,34 @@ export function AbsenceWatch() {
                 maximumAge: 0,
             }
         
-            const watchId = navigator.geolocation.watchPosition(
+            intervalWatchIdRef.current = navigator.geolocation.watchPosition(
                 successCallback,
                 errorCallback,
                 options
             )
-
-            setIntervalWatchId(watchId)        
         } else {
             console.error('Geolocation is not supported by your browser')
         }
     }
 
     useEffect(() => {
-        return () => navigator.geolocation.clearWatch(intervalWatchId)
-    },[intervalWatchId])
-
-    // useEffect(() => {
-            // if ('geolocation' in navigator) {
-            //     const successCallback = (position) => {
-            //     const { latitude, longitude } = position.coords
-            //     dispatch(setUserCoordinate([latitude,longitude]))
-            //     if (userCoordinate) focusOnLocation(userCoordinate)
-            // }
-        
-            // const errorCallback = (error) => {
-            //     console.log('Error getting geolocation', error)
-            // }
-        
-            // const options = {
-            //     enableHighAccuracy: true,
-            //     maximumAge: 0,
-            // }
-        
-            // const watchId = navigator.geolocation.watchPosition(
-            //     successCallback,
-            //     errorCallback,
-            //     options
-            // )
-        
-            // return () => navigator.geolocation.clearWatch(watchId)
-            // } else {
-            //     console.error('Geolocation is not supported by your browser')
-            // }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        // }, [dispatch])
-    useEffect(() => {
-        if ('geolocation' in navigator) {
-        } else {
-            console.error('Geolocation is not supported by your browser')
-        }
-        return () => navigator.geolocation.clearWatch(intervalWatchId)
-    })
-
-    return <div className="flex flex-col">
-        <button className='flex flex-1 shadow-lg shadow-primary/50 items-center justify-center rounded text-neutral-100 px-2 click-animation bg-secondary min-h-[32px] mt-auto' onClick={handleWatchPosition}>
-            {isWatchPosition ?
-                <LoadingIcon/>
-            :
-                <span>Absen</span>
+        return () => {
+            console.log('element unmount, so we shutdown watch gps')
+            if (intervalWatchIdRef.current) {
+                navigator.geolocation.clearWatch(intervalWatchIdRef.current)
             }
-        </button>
-    </div>
+        }
+    }, [])
+
+    return (
+        <div className="flex flex-col">
+            <button className='flex flex-1 shadow-lg shadow-primary/50 items-center justify-center rounded text-neutral-100 px-2 click-animation bg-secondary min-h-[32px] mt-auto' onClick={handleWatchPosition}>
+                {isWatchPosition ?
+                    <LoadingIcon/>
+                :
+                    <span>Absen</span>
+                }
+            </button>
+        </div>
+    )
 }
